b44819e0d81c6079b721663378fc863b1144c1b0
Rodrigo Martins <xkirtle26@gmail.com>
2023-12-23 16:22:23 +0000

    stuff

--- AOCUtils.cs
+++ AOCUtils.cs
@@ -1,4 +1,25 @@
-﻿internal static class AOCUtils
+﻿using LibGit2Sharp;
+
+internal static class AOCUtils
 {
+    private static async Task SetupProblemWithGit(int year, int day) {
+        // using var repo = new Repository(".git");
+        // var remote = repo.Network.Remotes["origin"];
+        // var branch = repo.Branches[$"problems/Y{year}/D{day}"];
+        // var branchExists = branch != null;
+        //
+        // branch ??= repo.Branches.Add($"problems/Y{year}/D{day}", $"origin/problems/Y{year}/D{day}", allowOverwrite: true);
+        // Commands.Checkout(repo, branch);
+        //
+        // if (!branchExists) {
+        //     // ...
+        // }
+    }
     
+    public static async Task SetupProblem(int year, int day, bool useGit = false) {
+        if (useGit)
+            await SetupProblemWithGit(year, day);
+        
+        //...
+    }
 }
--- Commands.cs
+++ Commands.cs
@@ -8,9 +8,23 @@ namespace AdventOfCode.NET;
 [DebuggerStepThrough]
 internal class Commands
 {
+    [Command(Description = "Initialize environment variables.")]
+    public void Init(
+        [Argument(Description = "AoC's session token.")] string session,
+        [Option("remote", ['r'], Description = "Your repository's remote branch.")] string remoteName = "origin", 
+        [Option("branch", ['b'], Description = "Your repository's default branch.")] string defaultBranch = "main"
+    )
+    {
+        Environment.SetEnvironmentVariable("AOC_SESSION_COOKIE", session);
+        
+        // TODO: Store these in an config file
+        Environment.SetEnvironmentVariable("AOC_GIT_REMOTE_NAME", remoteName);
+        Environment.SetEnvironmentVariable("AOC_GIT_DEFAULT_BRANCH", defaultBranch);
+    }
+    
     [Command]
-    public static void Setup(DateParameters date, [Option("no-git")] bool noGit) {
-        Console.WriteLine($"{date.Year} {date.Day} {noGit}");
+    public static Task Setup(DateParameters date, [Option("no-git")] bool noGit = false) {
+        return AOCUtils.SetupProblem(date.Year, date.Day, !noGit);
     }
 
     [Command]
