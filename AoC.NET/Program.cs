0d7666283abd9ee98085184fedbb83d03c044684
Rodrigo Martins <xkirtle26@gmail.com>
2023-12-24 19:38:55 +0000

    stuff

---
+++ AoC.NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using AoC.NET.Commands;
+using AoC.NET.Services;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSingleton<IHttpService, HttpService>();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var app = new CommandApp();
+app.Configure(config => {
+    config.AddCommand<SetupCommand>("setup")
+        .WithDescription("Setup a new problem")
+        .WithExample("setup", "2020", "15")
+        .WithExample("setup", "2020", "15", "--no-git", "true");
+});
+return app.Run(args);
---
+++ AoC.NET/Services/HttpService.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+
+namespace AoC.NET.Services;
+
+public interface IHttpService
+{
+    Task<object> FetchProblem(int year, int day);
+    Task<string> FetchProblemInput(int year, int day);
+}
+
+public class HttpService : IHttpService
+{
+    private readonly HttpClient _client;
+    private readonly Uri _aocBaseAddress;
+        
+    public HttpService() {
+        _aocBaseAddress = new Uri("https://adventofcode.com/");
+        var handler = new HttpClientHandler {
+            CookieContainer = new CookieContainer()
+        };
+
+        _client = new HttpClient(handler);
+        var aocSessionToken = GetSessionCookie();
+        
+        if (string.IsNullOrEmpty(aocSessionToken))
+            throw new InvalidOperationException("Session token not found. Please set the AOC_SESSION_TOKEN environment variable.");
+        
+        handler.CookieContainer.Add(_aocBaseAddress, new Cookie("session", aocSessionToken));
+    }
+    
+    public async Task<object> FetchProblem(int year, int day) {
+        var requestUri = new Uri(_aocBaseAddress + $"/{year}/day/{day}");
+        // Console.WriteLine("Updating " + requestUri, Color.Green);
+        var problem = await _client.GetAsync(requestUri);
+        
+        if (!problem.IsSuccessStatusCode) {
+            // Console.WriteLine($"Error downloading problem: {problem.StatusCode} {problem.ReasonPhrase}", Color.Red);
+            return null;
+        }
+
+        var input = await FetchProblemInput(year, day);
+        
+        // TODO: Parse problem description
+        
+        return await (Task<object>) Task.CompletedTask;
+    }
+    public async Task<string> FetchProblemInput(int year, int day) {
+        // TODO: Fetch problem input and parse response into a string
+        
+        // var input = await _client.GetAsync(requestUri + "/input");
+        //
+        // if (!input.IsSuccessStatusCode) {
+        //     // Console.WriteLine($"Error downloading input: {input.StatusCode} {input.ReasonPhrase}", Color.Red);
+        //     return null;
+        // }
+        
+        return await (Task<string>) Task.CompletedTask;
+    }
+
+    private static string GetSessionCookie() {
+        var sessionCookie = Environment.GetEnvironmentVariable("AOC_SESSION_COOKIE");
+        
+        if (string.IsNullOrEmpty(sessionCookie))
+            throw new InvalidOperationException("AOC_SESSION_COOKIE environment variable not set");
+        
+        return sessionCookie;
+    }
+}
